274fe392e2774953ddaec77ce1849bf19b280f67
Hiro Asari <asari.ruby@gmail.com>
2015-05-21 19:22:10 -0400

    Update RSpec syntax for jwt addon specs

--- spec/build/addons/jwt_spec.rb
+++ spec/build/addons/jwt_spec.rb
@@ -12,9 +12,9 @@ describe Travis::Build::Addons::Jwt, :sexp do
   describe 'jwt token, one secret' do
     let(:config) { 'MY_ACCESS_KEY=987654321' }
     it "should work" do
-      subject.should include_sexp [:echo, 'Initializing JWT', ansi: :yellow]
+      should include_sexp [:echo, 'Initializing JWT', ansi: :yellow]
       expected = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzbHVnIjoidHJhdmlzLWNpL3RyYXZpcy1jaSIsInB1bGwtcmVxdWVzdCI6IiIsImlhdCI6MH0.vHZdjuxQt6CdFmWPEJLAsVbI_KCxbwJVbgT7jaTxkK4"
-      subject.should include_sexp [:export, ['MY_ACCESS_KEY', expected]]
+      should include_sexp [:export, ['MY_ACCESS_KEY', expected]]
     end
   end
 
@@ -23,13 +23,13 @@ describe Travis::Build::Addons::Jwt, :sexp do
       secret1: 'MY_ACCESS_KEY_1=123456789',
       secret2: 'MY_ACCESS_KEY_2=ABCDEF'
     } }
-    it "should work" do
-      subject.should include_sexp [:echo, 'Initializing JWT', ansi: :yellow]
+    it {
+      should include_sexp [:echo, 'Initializing JWT', ansi: :yellow]
       expected1 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzbHVnIjoidHJhdmlzLWNpL3RyYXZpcy1jaSIsInB1bGwtcmVxdWVzdCI6IiIsImlhdCI6MH0.iO-I8CNXhQlgw_dL8R9CDaDPplSC3yf9J399Uumn6CE"
-      subject.should include_sexp [:export, ['MY_ACCESS_KEY_1', expected1]]
+      should include_sexp [:export, ['MY_ACCESS_KEY_1', expected1]]
       expected2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzbHVnIjoidHJhdmlzLWNpL3RyYXZpcy1jaSIsInB1bGwtcmVxdWVzdCI6IiIsImlhdCI6MH0.p1B9HnYCh-z5Igek5tr_QVPb1zV1ucwcPZNvY039WKg"
-      subject.should include_sexp [:export, ['MY_ACCESS_KEY_2', expected2]]
-    end
+      should include_sexp [:export, ['MY_ACCESS_KEY_2', expected2]]
+    }
   end
 end
 
